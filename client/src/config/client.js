69a31e5e834efe4ac906669490e98ebe05b9eb01
parasaraib <manikparasara@ymail.com>
2020-10-15 02:09:48 +0800

    add docker images

--- client/src/config/client.js
+++ client/src/config/client.js
@@ -3,7 +3,7 @@ import { ApolloClient, InMemoryCache, makeVar } from "@apollo/client";
 export const favorites = makeVar([]);
 
 const client = new ApolloClient({
-  uri: "http://3.1.221.149:80/",
+  uri: "http://localhost:4000",
   cache: new InMemoryCache({ typePolicies: {
     Query: {
       fields: {
